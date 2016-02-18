070945cc2c47f06d8803b7aa5239dae655a67a5f
Jonathan Bennett <jonathan@jbennett.me>
2016-02-17 22:16:01 -0500

    initial setup

---
+++ app/Timetable/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  Timetable
+//
+//  Created by Jonathan Bennett on 2016-02-17.
+//  Copyright © 2016 Jonathan Bennett. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+}
