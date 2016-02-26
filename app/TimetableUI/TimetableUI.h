36096a07a6dcc44e37f49a66932ee92665267d7d
Jonathan Bennett <jonathan@jbennett.me>
2016-02-26 14:04:06 -0500

    Add FloatLabelTextField

---
+++ app/TimetableUI/TimetableUI.h
@@ -0,0 +1,19 @@
+//
+//  TimetableUI.h
+//  TimetableUI
+//
+//  Created by Jonathan Bennett on 2016-02-26.
+//  Copyright © 2016 Jonathan Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimetableUI.
+FOUNDATION_EXPORT double TimetableUIVersionNumber;
+
+//! Project version string for TimetableUI.
+FOUNDATION_EXPORT const unsigned char TimetableUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimetableUI/PublicHeader.h>
+
+
